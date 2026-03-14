a21c6c68f6da000f635443c96bf46373b362ec1b
afaf500 <afafali7323@gmail.com>
2026-03-14 10:04:37 +0500

    Add files via upload

---
+++ challenge1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int number,even;
+    cout<<"Enter a number: ";
+    cin>>number;
+    even=number%2;
+    if(even==0)
+    {
+        cout<<"Number is even.";
+
+    }
+    else{
+        cout<<"Number is odd.";
+    }
+}
