97f2bbdd7e7a183e81dff5b20343909ce5928b40
afaf500 <afafali7323@gmail.com>
2026-03-14 10:05:43 +0500

    Add files via upload

---
+++ challenge2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int num1,num2;
+    cout<<"Enter number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    if(num1<num2)
+    {
+        cout<<"Number "<<num2<<" is greater than number "<<num1<<endl;
+    }
+    else
+    {
+        cout<<"Number"<<num1<<"is greater than number"<<num2<<endl;
+    }
+
+    
+}
